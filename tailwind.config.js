c94ea5251dc48c87127212c7fa679387b91f311a
tobi6112 <22715034+tobi6112@users.noreply.github.com>
2022-01-08 23:57:10 +0100

    introduce tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    `_site/**/*.html`,
+    './_layouts/**/*.html',
+    './_includes/**/*.html'
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
